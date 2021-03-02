b45657150860c829bd02b0000e699d87246ccdcb
Adil <amedeuyev@gmail.com>
2021-03-02 15:34:07 +0600

    Added ImagePicker to cells with changeable name

---
+++ Project10/Person.swift
@@ -0,0 +1,17 @@
+//
+//  Person.swift
+//  Project10
+//
+//  Created by Адиль Медеуев on 28.02.2021.
+//
+
+import UIKit
+
+class Person: NSObject {
+    var name: String
+    var image: String
+    init(name: String, image: String) {
+        self.name = name
+        self.image = image
+    }
+}
